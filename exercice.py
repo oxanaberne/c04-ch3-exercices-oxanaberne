9cfc49684a87060c725ccd817ca74bc224c7af2b
oxanaberne <oxana.berne@polymtl.ca>
2020-09-15 00:10:39 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -46,7 +46,7 @@ def to_farenheit(temperature: float) -> float:
 
 
 def main() -> None:
-    print(f"La racine carré de 144 est : {square_root(144)}")
+    print(f"La racine carré de 144 est : {square_root(8)}")
 
     print(f"Le carré de 12 est : {square(12)}")
 
