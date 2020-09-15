c6d93b814bf11b5ab2cc12159b4a85d0613d6b8b
oxanaberne <oxana.berne@polymtl.ca>
2020-09-15 01:09:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     angle_degs = angle_degs * math.pi / 180
-    angle_mins = angle_mins *
+    angle_mins = angle_mins * math.pi /
     return 0.0
 
 
