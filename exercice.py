cd084e44da09990e8f7d749c49998c764838625b
oxanaberne <oxana.berne@polymtl.ca>
2020-09-15 16:37:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -40,7 +40,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 def to_degrees(angle_rads: float) -> tuple:
     degrees = angle_rads * 180 / math.pi
     minutes = (degrees - int(degrees)) * 60)
-    seconds = * 60 / math.pi
+    seconds = (minutes - )
     return degrees, minutes, seconds
 
 
